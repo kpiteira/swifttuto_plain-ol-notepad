7daccb944fd754a4a55a46153c38266076314c26
Karl <kapiteir@microsoft.com>
2017-07-02 16:19:31 -0700

    Initial Commit

---
+++ Plain Ol' Notepad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plain Ol' Notepad
+//
+//  Created by Karl Piteira on 7/2/17.
+//  Copyright © 2017 Karl Piteira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
