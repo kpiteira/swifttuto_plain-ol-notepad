4b1d4c66f596c7fb4882bc73c72b274797deb5a1
Karl <kapiteir@microsoft.com>
2017-07-02 17:59:23 -0700

    Added a TableView with dummy content

--- Plain Ol' Notepad/ViewController.swift
+++ Plain Ol' Notepad/ViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource{
+    @IBOutlet weak var table: UITableView!
+    var tableData: [String] = ["Salut", "C'est", "moi!", "ou", "toi!"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell()
+        cell.textLabel?.text = tableData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
